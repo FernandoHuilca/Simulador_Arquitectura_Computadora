383ae26ec5ec76e1b4e62009e8832ff1679e5072
FernandoHuilca <fernando.huilca@epn.edu.ec>
2024-08-03 13:50:52 -0500

    Idea de interfaz gráfica

---
+++ GUI_Arquitectura_Computacional/src/gui_arquitectura_computacional/ArquitectuarBorrarController.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package gui_arquitectura_computacional;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Fernando_Huilca
+ */
+public class ArquitectuarBorrarController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
